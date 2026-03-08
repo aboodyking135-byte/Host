12f46036475e10fefa90ebc7589ab3bc93b63fd8
aboodyking135-byte <aboodyking135@gmail.com>
2026-03-08 14:29:59 +0200

    Create User.js

---
+++ User.js
@@ -0,0 +1,22 @@
+[
+  {
+    "id": 1,
+    "username": "DragonMaster",
+    "password": "Dragon@Host2026",
+    "isAdmin": true,
+    "createdAt": "2026-03-08T12:00:00.000Z",
+    "lastLogin": null,
+    "device": null,
+    "filesCount": 0,
+    "containers": [
+      {
+        "id": "container-1",
+        "name": "dragon-main",
+        "status": "running",
+        "ip": "10.0.0.1",
+        "cpu": "12%",
+        "memory": "512MB"
+      }
+    ]
+  }
+]
